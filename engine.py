d49ff1b1701a745ef30d3619d95a6cdc5d03ed4f
Brandon Ashworth <brandon@brandonashworth.com>
2012-06-07 22:37:20 -0700

    updated to support changes made in v0.10.0 of the core. Moved startup dir to tk-shotgun-launchhoudini

--- engine.py
+++ engine.py
@@ -20,8 +20,6 @@ import tank
 # application libs
 import hou
 
-CONSOLE_OUTPUT_WIDTH = 120
-
 class HoudiniEngine(tank.platform.Engine):
     def init_engine(self):
         self.log_debug("%s: Initializing..." % self)
@@ -30,34 +28,36 @@ class HoudiniEngine(tank.platform.Engine):
         # for the houdini engine (because it for example sets the houdini project)
         if len(self.context.entity_locations) == 0:
             # Try to create path for the context.
-            tank.platform.schema.create_filesystem_structure(self.shotgun,
-                                                           self.context.project_root,
-                                                           self.context.entity["type"],
-                                                           self.context.entity["id"])
+            self.tank.create_filesystem_structure(
+                self.context.entity["type"], 
+                self.context.entity["id"]
+            )
+            
             if len(self.context.entity_locations) == 0:
                 raise tank.TankError("No folders on disk are associated with the current context. The Houdini "
-                    "engine requires a context which exists on disk in order to run correctly.")
+                                "engine requires a context which exists on disk in order to run "
+                                "correctly.")
     
     def destroy_engine(self):
-        self.log_debug('%s: Destroying...' % self)
+        self.log_debug("%s: Destroying..." % self)
 
     def _display_message(self, msg):
         if hou.isUIAvailable():
             hou.ui.displayMessage(str(msg))
         else:
-            sys.stdout.write(str(msg)+'\n')
+            sys.stdout.write(str(msg)+"\n")
 
     def log_debug(self, msg):
-        sys.stdout.write(str(msg)+'\n')
+        sys.stdout.write(str(msg)+"\n")
 
     def log_info(self, msg):
-        sys.stdout.write(str(msg)+'\n')
+        sys.stdout.write(str(msg)+"\n")
 
     def log_error(self, msg):
         self._display_message(msg)
-        sys.stdout.write(str(msg)+'\n')
+        sys.stdout.write(str(msg)+"\n")
         
     def log_warning(self, msg):
-        sys.stdout.write(str(msg)+'\n')
+        sys.stdout.write(str(msg)+"\n")
 
 
