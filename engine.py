a2d4c1bac4cb12c9b6c5ec535195e243ca6a0bf8
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2015-10-25 08:30:05 -0400

    Add the dark look & feel back in for older versions of Houdini

--- engine.py
+++ engine.py
@@ -151,6 +151,9 @@ class HoudiniEngine(tank.platform.Engine):
                 app.setQuitOnLastWindowClosed(False)
                 app.setApplicationName(sys.argv[0])
 
+                # set the stylesheet
+                self._initialize_dark_look_and_feel()
+
             self.log_debug("No integrated PySide. Starting integrated event loop.")
             tk_houdini.python_qt_houdini.exec_(app)
 
