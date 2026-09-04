141b222309fe5c3821209b7f34ed6c6602256fd9
Steve Brown <steve.brown1@autodesk.com>
2026-09-04 12:31:05 +0100

    Changed min supported version of Houdini to 20.0

--- engine.py
+++ engine.py
@@ -24,7 +24,7 @@ import hou
 
 # Houdini versions compatibility constants
 VERSION_OLDEST_COMPATIBLE = (18, 5)
-VERSION_OLDEST_SUPPORTED = (19, 5)
+VERSION_OLDEST_SUPPORTED = (20, 0)
 VERSION_NEWEST_SUPPORTED = (22, 0)
 # Caution: make sure compatibility_dialog_min_version default value in info.yml
 # is equal to VERSION_NEWEST_SUPPORTED
